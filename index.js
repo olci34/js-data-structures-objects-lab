ae60891db73f80a7cc10fe4a0c1442ede923c4da
github username <github email address>
2021-04-16 22:46:22 -0400

    commit tree b873acdfbdf9ea59b52dd4c31a29541a08bbfecd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let driver = {awesome: "maan"}
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    const newObj = {}
+    newObj[key] = value
+    return Object.assign({},obj, newObj)
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    const newObj = {}
+    newObj[key] = value
+    return Object.assign(obj,newObj)
+}
+
+function deleteFromDriverByKey(obj, key) {
+    let newObj = { ...obj } // IF THIS WOULD BE const newObj = obj, THEN obj WOULD BE DESTRUCTIVELY CHANGED(pointers)
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
